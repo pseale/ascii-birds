1381e0eb2f90062e95923dc283e9b3947da7fb6b
Peter Seale <peter@pseale.com>
2014-04-29 15:30:38 -0500

    add start and quit buttons

--- site/main.js
+++ site/main.js
@@ -1,3 +1,27 @@
-function test() {
-  return 5;
+function startGame() {
+  $('#title').hide();
+  $('#start-button').hide();
+  $('#screen').show();
+  $('#quit-button').show(); 
 }
+
+function quitGame() {
+  $('#title').show();
+  $('#start-button').show();
+  $('#screen').hide();
+  $('#quit-button').hide(); 
+}
+
+//jQuery page bindings
+$(document).ready(function() {
+  $('#start-button').click(function() {
+    startGame();
+    return false;
+  });  
+
+  $('#quit-button').click(function() {
+    quitGame();
+    return false;
+  });  
+});
+
