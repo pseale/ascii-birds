9e77ce9d1dff496c058e413d33e178c6f0e1a54d
Peter Seale <peter@pseale.com>
2014-05-01 03:19:23 -0500

    add specs for home page

--- spec/features/starting_spec.rb
+++ spec/features/starting_spec.rb
@@ -1,14 +1,23 @@
 require_relative '../spec_helper'
 
 describe 'Visiting the site fresh' do
-  it 'should have a START button' do
+  before :each do
     visit '/'
+  end
+
+  it 'should have a START button' do
     expect(page.has_link? "START").to be_true
   end
 
-  it 'should NOT have a QUIT button' do
-    visit '/'
+  it 'should have a title screen' do
+    expect(find('#title').visible?).to be_true
+  end
 
+  it 'should not show the game screen' do
+    expect(find('#screen', :visible=>false).visible?).to be_false
+  end
+
+  it 'should NOT have a QUIT button' do
     expect(page).to_not have_selector('div#quit-button', :visible=>true)
   end
 end
