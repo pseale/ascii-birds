6968d0f600cba8b53099822af4eddcece4c1f516
Peter Seale <peter@pseale.com>
2014-05-01 02:50:35 -0500

    separate out jQuery from core JS

---
+++ site/index.html.js
@@ -0,0 +1,32 @@
+function () {
+
+function startGame() {
+  $('#title').hide();
+  $('#start-button').hide();
+  $('#screen').show();
+  $('#quit-button').show(); 
+}
+
+function quitGame() {
+  $('#title').show();
+  $('#start-button').show();
+  $('#screen').hide();
+  $('#quit-button').hide(); 
+}
+
+//jQuery page bindings
+$(document).ready(function() {
+  $('a#start-button').click(function() {
+    startGame();
+    return false;
+  });  
+
+  $('a#quit-button').click(function() {
+    quitGame();
+    return false;
+  });  
+});
+
+
+
+}();
