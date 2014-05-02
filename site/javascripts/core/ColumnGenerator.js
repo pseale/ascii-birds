b864685fe6f79babf6e83bfe656e1896349222ee
Peter Seale <peter@pseale.com>
2014-05-02 00:33:31 -0500

    refactor: move js files

---
+++ site/javascripts/core/ColumnGenerator.js
@@ -0,0 +1,11 @@
+var ColumnGenerator = Class.extend({
+  init: function() {
+    this.offset = 0;
+  },
+
+  next: function() {
+    this.offset += _.random(7, 10);
+
+    return this.offset;
+  },
+});
--- site/javascripts/core/Game.js
+++ site/javascripts/core/Game.js
@@ -1,18 +1,3 @@
-
-//----------------------------------------------------------------------------
-
-var ColumnGenerator = Class.extend({
-  init: function() {
-    this.offset = 0;
-  },
-
-  next: function() {
-    this.offset += _.random(7, 10);
-
-    return this.offset;
-  },
-});
-
 var Game = Class.extend({
   init: function() {
     this.topColumnGenerator = new ColumnGenerator();
